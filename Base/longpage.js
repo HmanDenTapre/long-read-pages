5341d754c6c9026f63107af54243ef369be1284b
HmanDenTapre <hermylearm@gmail.com>
2025-05-19 12:39:01 +0200

    initial

---
+++ Base/longpage.js
@@ -0,0 +1,20 @@
+
+document.querySelectorAll('.background').forEach(bgimg => {
+    const imgs = bgimg.querySelectorAll('img');
+    imgs.forEach((img, index) => {
+      bgimg.style.setProperty(`--img${index + 1}`, `url(${img.src})`);
+      img.style.display = 'none'; 
+    });
+});
+
+/*
+document.querySelectorAll("img.frame-cycle").forEach(img => {
+    const frames = img.dataset.frames.split(",");
+    let i = 0;
+  
+    setInterval(() => {
+      i = (i + 1) % frames.length;
+      img.src = frames[i].trim();
+    }, 200); // 200ms per frame
+});
+*/
